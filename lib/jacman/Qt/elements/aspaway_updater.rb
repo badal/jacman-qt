11c55dc8f3829ca441be700edb8254b8f683f0e9
Michel Demazure <michel@demazure.com>
2014-09-19 14:54:34 +0200

    Fixed Core constants

--- lib/jacman/Qt/elements/aspaway_updater.rb
+++ lib/jacman/Qt/elements/aspaway_updater.rb
@@ -19,7 +19,7 @@ module JacintheManagement
       def initialize(source_file, call_name)
         @importer = Core::AspawayImporter.new(source_file)
         @source_age = nil
-        @stamp = File.join(CRON_DIR, "stdout_#{call_name}.txt")
+        @stamp = File.join(Core::Command::CRON_DIR, "stdout_#{call_name}.txt")
         @call_name = call_name
       end
 
